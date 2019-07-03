79a6506d42812247ecc44d5f3e6bd59d239c9a92
excelsior <excelsior1984@o2.pl>
2019-07-03 07:45:08 +0200

    part2 dockerizing jenkins

---
+++ test_file_upload.sh
@@ -0,0 +1,2 @@
+curl -uadmin:password -T file-1.0.0.zip http://localhost:8081/artifactory/example-repo-local/file-1.0.0.zip
+#curl -uadmin:AP8H212EPQnrsDuJxhQVG24MRH6 -T file-1.0.0.zip http://localhost:8081/artifactory/example-repo-local/file-1.0.0.zip
